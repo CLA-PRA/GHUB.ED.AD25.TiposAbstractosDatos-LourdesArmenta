125856e4503f3556e2acc4de878cf77e28b18ccf
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-09-05 00:50:05 +0000

    con un cambio en la prueba

--- miTest/AppTest.java
+++ miTest/AppTest.java
@@ -235,9 +235,9 @@ class AppTest {
         cadena.agregarFinal('C');
         cadena.agregarFinal('O');
         cadena.borrarInicio();
-        assertTrue(cadena.getCadena()[0]=='M');
+        assertTrue(cadena.getCadena()[0]=='E');
 
-        assertTrue(cadena.tamanio()==6);
+        assertTrue(cadena.tamanio()==5);
 
     }
 
